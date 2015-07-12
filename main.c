703a0ad613a80abe395af4fe1919dec311c7b81b
Victor-Nicolae Savu <victor.nicolae.savu@gmail.com>
2015-07-12 09:41:53 +0200

    Removed unnecessary includes in main.c

--- main.c
+++ main.c
@@ -1,25 +1,10 @@
-#include <errno.h>
-#include <locale.h>
-#include <stdarg.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <X11/cursorfont.h>
-#include <X11/keysym.h>
-#include <X11/Xatom.h>
-#include <X11/Xlib.h>
-#include <X11/Xproto.h>
-#include <X11/Xutil.h>
-#ifdef XINERAMA
-#include <X11/extensions/Xinerama.h>
-#endif /* XINERAMA */
-#include <X11/Xft/Xft.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#include "drw.h"
+#include <X11/Xlib.h>
+
 #include "util.h"
 #include "lib.h"
 
